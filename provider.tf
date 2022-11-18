66f7cb8e80ceb451d91ec896bd4b40b064d04eb6
Saurav Thakur <91631978+Saurav0310@users.noreply.github.com>
2022-11-18 13:25:35 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,6 +10,5 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key= "AKIARF43ZCV3IFN34JZD"
-  secret_key= "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"
+  profile = "saurav"
 }
