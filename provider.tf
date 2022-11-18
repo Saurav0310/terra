2cc1c51d18057b33890bfce577e32e9a226cfeb2
Saurav Thakur <91631978+Saurav0310@users.noreply.github.com>
2022-11-18 14:49:12 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,5 +10,5 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  role_arn=arn:aws:iam::081394210166:role/adminrole
+  role_arn="arn:aws:iam::081394210166:role/adminrole"
 }
