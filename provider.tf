60961e67a9e1e899d5d7d3e9b6fde13e1efd7857
Saurav Thakur <91631978+Saurav0310@users.noreply.github.com>
2022-11-18 15:18:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,6 +7,57 @@ terraform {
   }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
